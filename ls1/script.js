2bcdcf9495245ba2702866470501ba684a06afa5
AnhHT3010 <trunganhhoang2001@gmail.com>
2024-09-22 18:23:04 +0700

    JS intro

---
+++ ls1/script.js
@@ -0,0 +1,34 @@
+// built-in:
+// console.log(15 + 15);
+// alert(1 + 1);
+
+// let x = 2;
+// x = 6;
+// console.log(x);
+// console.log(x % 2 == 0);
+// console.log(x / 4);
+// khai báo biến: const, let
+
+// quy tắc đặt tên biến
+// 1. Không bắt đầu bằng số
+// 2. Không chứa ký tự đặc biệt
+// 3. Không trùng với từ khóa của ngôn ngữ
+// 4. Đặt tên biến có ý nghĩa
+
+// operator: +, -, *, /, %, ++, --
+// console.log(++x);
+// x= x + 1
+// x+= 1
+// x++
+
+// data-type:
+// 1. string: chuỗi ký tự (các chữ hoặc các từ)
+// console.log("xin chào");
+// 2. number: số ( số nguyên, số thực )
+// 3. boolean: true, false
+// let check = true;
+// 4. null: không có giá trị
+// 5. undefined: chưa được khai báo
+// console.log(typeof x); // kiểm tra kiểu dữ liệu của biến
+
+// Toán tử so sánh, toán tử logic
