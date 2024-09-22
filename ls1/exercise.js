7603ec007d7103d7c02f41c44c0ac835a84961b4
AnhHT3010 <trunganhhoang2001@gmail.com>
2024-09-22 18:37:47 +0700

    btap

---
+++ ls1/exercise.js
@@ -0,0 +1,6 @@
+// 0. In ra thông tin lời chào và giới thiệu bản thân.
+// 1. Tính tổng của hai số                                VD: Tổng của 5 và 7 là: 12
+// 2. Tính diện tích hình tròn                            VD: Diện tích hình tròn với bán kính 3 là: 28.274333882308138
+// 3. Tính chỉ số BMI                                     VD: Chỉ số BMI của bạn là: 22.86 (biết cân nặng bạn đó là 70kg và chiều cao là 1.75m)
+// 4. Tính thể tích hình trụ                              VD: Thể tích hình trụ với bán kính 3 và chiều cao 5 là: 141.3716694115407
+// 5. In chuyển đổi nhiệt độ từ Celsius sang Fahrenheit   VD: Nhiệt độ 25°C là 77.0°F
