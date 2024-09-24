0a996c1114d7937b0cb680da084444136684a096
Khang <lehoainhatkhang2008@gmail.com>
2024-09-24 17:59:48 +0700

    ...

--- ls1/exercise.js
+++ ls1/exercise.js
@@ -4,3 +4,28 @@
 // 3. Tính chỉ số BMI                                     VD: Chỉ số BMI của bạn là: 22.86 (biết cân nặng bạn đó là 70kg và chiều cao là 1.75m)
 // 4. Tính thể tích hình trụ                              VD: Thể tích hình trụ với bán kính 3 và chiều cao 5 là: 141.3716694115407
 // 5. In chuyển đổi nhiệt độ từ Celsius sang Fahrenheit   VD: Nhiệt độ 25°C là 77.0°F
+//1
+// x = Number(prompt("Số 1"))
+// y = Number(prompt("Số 2"))
+// result = x + y
+// console.log("Tổng =",result )
+//2 
+//   x = Number(prompt("Bán kính"))
+//   area = x **2 * 3.14
+//   console.log("Area =",area)
+//3
+// Height = Number(prompt("Chiều cao"))
+// Weight = Number(prompt("Cân nặng"))
+// bmi = Weight / (Height **2)
+// console.log("Bmi =", bmi)
+// console.log(Weight)
+// console.log(Height)
+//4
+//  Radius = Number(prompt("Bán kính"))
+//  Height = Number(prompt("Chiều cao"))
+//  v = Radius **2 * 3.14 * Height
+//  console.log("V = ", v)
+//5
+//    Celsius = Number(prompt("Độ:"))
+//    f = Celsius * 1.8 + 32
+//    console.log("Fahrenheit =", f)
