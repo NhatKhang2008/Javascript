e76a12140c55a9ba4416288c34071a6086cd4e60
Khang <lehoainhatkhang2008@gmail.com>
2024-09-24 20:06:52 +0700

    sth

--- ls2/exercise.js
+++ ls2/exercise.js
@@ -1,3 +1,36 @@
 // B1: Kiểm tra số chẵn lẻ
 // B2: Tìm số lớn nhất trong 3 số
 // B3: Giải phương trình bậc nhất ax + b = 0 ( với a != 0)
+// b1
+// number = Number(prompt("Number"))
+// if ( number % 2 == 0) {
+//     console.log(`Number ${number} la so chan`)
+
+// } else{
+//     console.log(`Number ${number} la so le`)
+// }
+// b2
+//  number1 = Number(prompt("First number"))
+//  number2 = Number(prompt("Second number"))
+//  number3 = Number(prompt("Third number"))
+//   if ( number1 > number2 && number1 > number3){
+//         console.log(`Number ${number1} la so lon nhat`)
+//     }else if ( number2 > number1 && number2 > number3){
+//         console.log(`Number ${number2} la so lon nhat`)
+//     }else if ( number3 > number2 && number3 > number1){
+//         console.log(`Number ${number3} la so lon nhat`)
+//     }else {
+//         console.log("Hay dua ba so khac nhau")
+//     }
+// b3
+// a = Number(prompt("Number"))
+// b = Number(prompt("Number"))
+// x = -b/a 
+// if (a == 0){
+//     console.log("Hay cho mot so a khac khong")
+// }else{
+//     console.log(`x la ${x}`)
+// }
+
+
+
