963264ca526e73de202094cddaf48b24e6ccb9d3
AnhHT3010 <trunganhhoang2001@gmail.com>
2024-10-08 23:09:46 +0700

    bai-tap

--- ls4/exercise.js
+++ ls4/exercise.js
@@ -5,18 +5,28 @@
 // Bài tập 2: Đếm số lần xuất hiện của một phần tử trong mảng. Biết rằng mảng và phần tử cần kiểm tra được nhập từ bàn phím.
 // Ví dụ:
 // cho mảng number = [1, 23, 33, 41, 33, 42, 1, 22, 23, 23] => kết quả: nếu nhập số 23 xuất hiện 3 lần
-//BT1 
-// const number1 = [10, -12, 0, -1, -3, -4 ];
-// let Max = number1[0];
-//  for ( i = 1; i < number1.length; i++) {
-//     if (number1[i] > Max) (Max = number1[i])
-//  }
-// console.log(`So lon nhat la ${Max}`)
-//BT2
-//  const number1 =  [1, 23, 33, 41, 33, 42, 1, 22, 23, 23];
-//  n = Number(prompt("So?"));
-//  let times = 0;
-//  for ( i = 0 ; i < number1.length; i++) {
-//     if ( number1[i] == n) {times++}
-//  }
-// console.log(`So lan xuat hien so ${n} la ${times} lan`)
+
+// Bài tập 3: Đảo ngược thứ tự bài hát trong danh sách phát
+// Yêu cầu: Bạn đang quản lý một danh sách phát nhạc (playlist).
+// Sau khi người dùng nghe qua tất cả các bài hát, họ muốn nghe lại từ bài hát cuối cùng đến bài hát đầu tiên. Hãy đảo ngược thứ tự bài hát trong danh sách phát.
+// let playlist = ["Song A", "Song B", "Song C", "Song D", "Song E"]; => kết quả: ["Song E", "Song D", "Song C", "Song B", "Song A"]
+
+// Bài tập 4: Kiểm tra mảng có phải mảng tăng dần không
+// Yêu cầu: Kiểm tra xem mảng có phải mảng tăng dần không. Nếu có trả về true, ngược lại trả về false.
+// Ví dụ:
+// const number5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; => kết quả: true
+// const number6 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 8]; => kết quả: false
+
+// Bài tập 5: Kiểm tra mảng có phải mảng "symmetric" không
+// Yêu cầu: Kiểm tra xem mảng có phải mảng "symmetric" không. Mảng "symmetric" là mảng đối xứng qua trục đứng giữa mảng.
+// Ví dụ:
+// const number7 = [1, 2, 3, 4, 5, 4, 3, 2, 1]; => kết quả: true
+// const number8 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; => kết quả: false
+
+// Bài tập 6: Tìm số lớn thứ 2 trong mảng
+// Ví dụ:
+// const number9 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; => kết quả: 9
+// const number10 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]; => kết quả: 9
+// const number11 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10]; => kết quả: 9
+// const number12 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 10]; => kết quả: 9
+// const number13 = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]; => kết quả: không có số lớn thứ 2
