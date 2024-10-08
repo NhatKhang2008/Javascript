78bbc170a35d0996075439bc48c031c994876029
AnhHT3010 <trunganhhoang2001@gmail.com>
2024-10-08 19:50:17 +0700

    array

--- ls4/exercise.js
+++ ls4/exercise.js
@@ -1,7 +1,7 @@
-// Bài tập 1: Đếm số lần xuất hiện của một phần tử trong mảng. Biết rằng mảng và phần tử cần kiểm tra được nhập từ bàn phím.
-// Ví dụ:
-// cho mảng number = [1, 23, 33, 41, 33, 42, 1, 22, 23, 23] => kết quả: nếu nhập số 23 xuất hiện 3 lần
-// Bài tập 2: Tìm số lớn nhất trong mảng
+// Bài tập 1: Tìm số lớn nhất trong mảng
 // Ví dụ:
 // const number3 = [10, -12, 0, 1, 3, 4, 1, 23, 101, 100, 19];         => kết quả: 101
 // const number4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];                    => kết quả: 10
+// Bài tập 2: Đếm số lần xuất hiện của một phần tử trong mảng. Biết rằng mảng và phần tử cần kiểm tra được nhập từ bàn phím.
+// Ví dụ:
+// cho mảng number = [1, 23, 33, 41, 33, 42, 1, 22, 23, 23] => kết quả: nếu nhập số 23 xuất hiện 3 lần
